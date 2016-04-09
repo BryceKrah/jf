4d366414df8757566197f429f789e2e13f06df86
Bryce Krah <krahbryce@yahoo.com>
2016-04-09 19:01:24 -0400

    installed all dependencies and bundled js, set up initial app js componoent

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+const Router = ReactRouter.Router;
+const browserHistory = ReactRouter.browserHistory;
+const Route = ReactRouter.Route;
+const Link = ReactRouter.Link;
+
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+), document.querySelector('#container'));
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict'
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/', (req,res)=>{
-  res.send("working")
-})
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const port = process.env.PORT || 3000;
