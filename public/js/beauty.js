32c4435ebe4d7b6234d36bc1bf45856711e0e08f
Bryce Krah <krahbryce@yahoo.com>
2016-04-18 17:43:45 -0400

    added coverflow react comp to bridal and beauty page

--- public/js/beauty.js
+++ public/js/beauty.js
@@ -4,13 +4,33 @@ const ReactRouter = require('react-router');
 const Link = ReactRouter.Link;
 const Nav = require('./nav.js');
 const Footer = require('./footer.js')
+const Coverflow = require('react-coverflow')
+
 
 const Beauty = React.createClass({
   render: function(){
     return (
       <div>
         <Nav />
-        <h1>beauty stuff</h1>
+        <h1>Beauty Portfolio</h1>
+        <Coverflow width="960" height="500"
+          displayQuantityOfSide={1}
+          navigation={true}
+          enableScroll={true}
+        >
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://lcc1009.cias.rit.edu/256/homework1/images/frenchy.jpg' alt='title or description' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://lcc1009.cias.rit.edu/256/homework1/images/frenchy.jpg' alt='title or description' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://lcc1009.cias.rit.edu/256/homework1/images/frenchy.jpg' alt='title or description' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          <img src='http://lcc1009.cias.rit.edu/256/homework1/images/frenchy.jpg' alt='title or description' />
+          <img src='http://previews.123rf.com/images/ivonnewierink/ivonnewierink1403/ivonnewierink140300315/26969955-French-bulldog-standing-in-studio-on-beige-background-Stock-Photo.jpg' alt='Frenchie pup' />
+          </Coverflow>
         <Footer />
       </div>
     )
---
+++ public/js/coverflow.js
@@ -0,0 +1,3 @@
+'use strict';
+const React = require('react');
+const CoverFlow = require('react-coverflow')
