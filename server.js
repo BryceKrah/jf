8423c4fd09f7528bd36448f90e13b19d79080c7a
Bryce Krah <krahbryce@yahoo.com>
2016-08-10 20:40:18 -0400

    attempt at caching, also added meta viewport setting

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const morgan = require('morgan');
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/*', (req, res, next)=> {
+console.log("req.url",req.url);
+  if (req.url.indexOf("/icon_imgs/") === 0 || req.url.indexOf("/css/") === 0) {
+    res.setHeader("Cache-Control", "public, max-age=2592000");
+    res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+  }
+  next();
+})
 
 app.get('/', (req,res)=>{
   res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -17,6 +26,6 @@ app.get('*', (req,res)=>{
 })
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, ()=>{
+http.createServer(app).listen(port, ()=>{
   console.log("listening on port: ", port);
 })
